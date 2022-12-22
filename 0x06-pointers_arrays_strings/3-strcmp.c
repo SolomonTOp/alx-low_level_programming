eadc63841986905fa15d6e29cb6390101782622d
SolomonTOp <coffeetube2@gmail.com>
2022-12-22 09:04:34 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,7 +20,7 @@ int _strcmp(char *s1, char *s2)
 			break;
 		else if (s1[i] == '\0')
 		{
-			diff = s2 [i];
+			diff = s2[i];
 			break;
 		}
 		else if (s2[i] == '\0')
@@ -35,6 +35,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 			i++;
+
 	}
 	return (diff);
 }
