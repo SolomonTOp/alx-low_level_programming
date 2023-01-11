ca38a33da31132bf7c557d5daa033f36b89eeeb0
SolomonTOp <coffeetube2@gmail.com>
2023-01-11 00:58:13 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - write a program that prints its name, followed by a new line.
+ * main - Write a program that prints its name, followed by a new line.
  * If you rename the program, it will print the new name, without having to compile it again
  * You should not remove the path before the name of the program
  *
