bfbd60a6e07f9d03c5d99d4434c3c141a3b19606
SolomonTOp <coffeetube2@gmail.com>
2023-01-16 03:34:01 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,7 +28,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	n_dog->name = malloc(lenN + 1);
-	n_dog->name = malloc(lenN + 1);
 	if (n_dog->name == NULL)
 	{
 		free(n_dog->name);
